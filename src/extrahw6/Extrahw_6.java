04003008a5a2ac0efcf1c9fb3ef6bda6452047ca
dipamr <pateldipali915@gmail.com>
2022-12-21 15:24:11 +0000

    ExtraHW_6

---
+++ src/extrahw6/Extrahw_6.java
@@ -0,0 +1,26 @@
+package extrahw6;
+
+public class Extrahw_6 {
+    public static void main(String[] args) {
+        String a = "I love java";
+        String b = "I eat java";
+        String c = "Dipali";
+        String d = "Patel";
+
+
+        System.out.println(a.replace("I","we"));
+        System.out.println(a.length());
+        System.out.println(a+b);
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.replace("I","we"));
+        System.out.println(a.replace("@"," "));
+        System.out.println(a.replace("I","we"));
+        System.out.println(a.trim());
+        System.out.println("Dipali\npatel");
+
+
+
+
+    }
+}
